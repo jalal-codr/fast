67ff5a03614295ea7357be125ded7070710ebfd2
Mohammed Abduljalal <abduljalal849@gmai.com>
2023-02-21 09:55:42 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+app.use(express.json());
+
+const arr = ['hello']
+
+app.get('/',(req,res)=>{
+    res.send('hello world')
+})
+
+
+app.post ('/post',(req,res)=>{
+    const person = {
+        name : req.body.name,
+        age : req.body.age
+    }
+    arr.push(person)
+    console.log('person saved')
+})
+
+
+app.get('/post',(req,res)=>{
+    res.send(arr)
+})
+
+
+
+const port = 7000
+app.listen(port,()=> console.log(`server runing on ${port}`));
